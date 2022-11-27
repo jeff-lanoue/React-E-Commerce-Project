7fad7b87e5aa80368fd7b7f80c136b67af9c7b8c
Jeff Lanoue <jeff.lanoue.1@gmail.com>
2022-11-26 19:36:03 -0800

    Added routes, set up nav, footer and sidebar

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import CheckOut from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+    Home,
+    Products,
+    SingleProduct,
+    About,
+    Cart,
+    Error,
+    CheckOut,
+    PrivateRoute,
+};
